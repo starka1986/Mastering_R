819ed94054975eb218576f8c36c207add35a4b13
starka1986 <agnes.stark@gmail.com>
2020-04-15 12:41:53 +0000

    add logging

--- cypto_report.R
+++ cypto_report.R
@@ -2,12 +2,13 @@ library(binancer)
 library(lubridate)
 library(jsonlite)
 library(httr)
+library(logger)
 prices <- binance_ticker_all_prices()
 prices[from == 'BTC' & to == 'USDT', price]
 
 binance_coins_prices() [symbol == 'BTC', usd]
 
-print(paste0('Value of .42 Bitcoins is $', round(0.42 * binance_coins_prices()[symbol == 'BTC']$usd, 2)))
+#print(paste0('Value of .42 Bitcoins is $', round(0.42 * binance_coins_prices()[symbol == 'BTC']$usd, 2)))
 
 # get url
 
@@ -16,11 +17,23 @@ readLines('https://api.exchangeratesapi.io/latest?base=USD')
 # parse json
 # could use only JSONlite fromJSON no need for readLines
 
-usdhuf <- fromJSON('https://api.exchangeratesapi.io/latest?base=USD&symbola=HUF')$rates$HUF
+
 usdhuf
 
 # getting current bitcoin price and converting 
 
+# create a constant on top
+BITCOINS <-0.42 
+log_info('Number of Bitcoins: {BITCOINS}') # glue package
 
-0.42 * binance_coins_prices() [symbol == 'BTC', usd] * fromJSON('https://api.exchangeratesapi.io/latest?base=USD&symbola=HUF')$rates$HUF
+## TODO the Binance API is a bit of mess... need to add retries
+
+btcusdt <- binance_coins_prices() [symbol == 'BTC', usd]
+log_info('The value of 1 Bitcoin in USD : {btcusdt}')
+
+# create object for huf conversion
+usdhuf <- fromJSON('https://api.exchangeratesapi.io/latest?base=USD&symbola=HUF')$rates$HUF
+
+
+BITCOINS * btcusdt * usdhuf
 
